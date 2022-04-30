85fad7026310dd46d3b17f6b1ee1271b3e1ffc5e
alextrevg <65139263+alextrevg@users.noreply.github.com>
2022-04-30 00:14:19 -0500

    Add files via upload

---
+++ EvidenciaApps_AETG/EvidenciaApps_AETG/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  EvidenciaApps_AETG
+//
+//  Created by user188676 on 12/3/21.
+//  Copyright © 2021 user188676. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var MTHLabel: UILabel!
+    @IBOutlet weak var LoginLabel: UILabel!
+    @IBOutlet weak var UserField: UITextField!
+    @IBOutlet weak var PassField: UITextField!
+    @IBAction func BtnIngresar(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EvidenciaApps_AETG/EvidenciaApps_AETG/ViewControllerTwo.swift
@@ -0,0 +1,49 @@
+//
+//  ViewControllerTwo.swift
+//  EvidenciaApps_AETG
+//
+//  Created by user188676 on 12/3/21.
+//  Copyright © 2021 user188676. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    
+    
+    @IBAction func BtnYTIdentidad(_ sender: Any) {
+    }
+    
+    @IBAction func BtnYTMth(_ sender: Any) {
+    }
+    
+    @IBAction func BtnFacebook(_ sender: Any) {
+    }
+    
+    
+    @IBAction func BtnInstagram(_ sender: Any) {
+    }
+    
+    @IBAction func BtnTiktok(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
